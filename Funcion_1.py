7e04295149c9b36d8590086400e990af480706d3
Evelynmaezj <a01733906@tec.mx>
2024-10-31 15:36:58 -0600

    Creación de rama backend

--- Funcion_1.py
+++ Funcion_1.py
@@ -3,3 +3,8 @@ print("Comenzar con las modificaciones")
 a = 45
 b = 56
 print(a+b)
+
+#Crear las funciones de backend 
+def mostrar():
+    print("Esta rama esta designada para los cambios necesarios en el backend")
+mostrar()
